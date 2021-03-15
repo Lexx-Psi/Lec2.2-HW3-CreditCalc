d3db6781f0c66ccce3ff64ca274444c66e2b1aab
Lexx-Psi <60262142+Lexx-Psi@users.noreply.github.com>
2021-03-15 16:33:40 +0300

    Add files via upload

---
+++ CreditPaymentService.java
@@ -0,0 +1,12 @@
+import static java.lang.Math.*;
+
+public class CreditPaymentService {
+    public float calculate(float percent, float month) {
+        int debt = 1000000;
+        float bodycreditsumm = debt/month;
+        float monthpercentdiff = (100 * percent)/debt;
+        float contribution;
+        contribution = monthpercentdiff + bodycreditsumm;
+        return (contribution);
+    }
+}
